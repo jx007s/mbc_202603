b513063ddcd261d3a1251c1d19240e7264cd2fbb
nemo <jx007s@gmail.com>
2026-04-07 16:26:29 +0900

    4_7_5

--- java_work/teacherPrj/src/oops_p/MeCarMain.java
+++ java_work/teacherPrj/src/oops_p/MeCarMain.java
@@ -8,6 +8,15 @@ import java.util.Arrays;
 //메소드를 이용하여 계산하세요 - 입력부, 연산부, 출력부
 //차종, 속도, 연비, 안정성, 성능, 결과
 //차종 5개 이상
+
+class MeCarProvider{
+	MeCar make(String title, int speed, int fuel, int safe) {
+		MeCar ret = new MeCar();
+		ret.input(title, speed, fuel, safe);
+		return ret;
+	}
+}
+
 class MeCar{
 	String title;
 	int [] jum;
@@ -50,6 +59,7 @@ class MeCar{
 public class MeCarMain {
 
 	public static void main(String[] args) {
+		/*
 		MeCar [] cars = new MeCar[5];
 		for (int i = 0; i < cars.length; i++) {
 			cars[i] = new MeCar();
@@ -59,6 +69,16 @@ public class MeCarMain {
 		cars[2].input("왜그랬때", 77,78,76);
 		cars[3].input("그랜져", 87,89,85);
 		cars[4].input("안그랜져", 65,67,62);
+		*/
+		
+		MeCarProvider provider = new MeCarProvider();
+		MeCar [] cars = {
+			provider.make("아반떼", 96, 71, 52),
+			provider.make("삼각떼", 56, 73, 99),
+			provider.make("왜그랬때", 77,78,76),
+			provider.make("그랜져", 87,89,85),
+			provider.make("안그랜져", 65,67,62)	
+		};
 		
 		for (MeCar car : cars) {
 			car.ppp();
