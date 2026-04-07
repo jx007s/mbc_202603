a02b47f3716507dec4339a8ad657c6089d716fe7
nemo <jx007s@gmail.com>
2026-04-07 12:04:32 +0900

    4_7_2

--- java_work/teacherPrj/src/oops_p/MeStudMain.java
+++ java_work/teacherPrj/src/oops_p/MeStudMain.java
@@ -39,6 +39,7 @@ class MeClaStud{
 public class MeStudMain {
 
 	public static void main(String[] args) {
+		/*
 		MeClaStud st1 = new MeClaStud();
 		MeClaStud st2 = new MeClaStud();
 		MeClaStud st3 = new MeClaStud();
@@ -49,7 +50,45 @@ public class MeStudMain {
 		st1.ppp();
 		st2.ppp();
 		st3.ppp();
+		
+		
+		MeClaStud [] studs = new MeClaStud[]{
+			new MeClaStud(),
+			new MeClaStud(),
+			new MeClaStud()
+		};
+		*/
+		
+		MeClaStud [] studs = new MeClaStud[3];
+		for (int i = 0; i < studs.length; i++) {
+			studs[i] =new MeClaStud();
+		}
+		
+		studs[0].input("현빈", 78, 73, 79);
+		studs[1].input("원빈", 88, 83, 89);
+		studs[2].input("투빈", 68, 63, 69);
+		
+		for (MeClaStud st : studs) {
+			st.ppp();
+		}
+		
 
 	}
 
 }
+
+
+//자동차 성능 테스트 클래스를 완성하세요
+//성능 : 속도(60%) + 연비(30%) + 안정성(10%)
+//결과 : 평점 80 이상 -  우수, 70이상 - 보통 , 나머지 - 불량
+
+//메소드를 이용하여 계산하세요 - 입력부, 연산부, 출력부
+//차종, 속도, 연비, 안정성, 성능, 결과
+// 차종 5개 이상
+
+
+
+
+
+
+
