cd726df8906059255c9ca7457f9722ad94a9620a
nemo <jx007s@gmail.com>
2026-05-07 13:00:51 +0900

    5_7_3

--- db_sql/01_user_db.sql
+++ db_sql/01_user_db.sql
@@ -13,9 +13,31 @@ create DATABASE busan_db;
 -- 권한부여
 alter database busan_db owner to busan_user;
 
+
+-- database 삭제
+drop DATABASE busan_db;
+
+-- user 삭제
+drop USER busan_user;
+
+
+
+
+
+-- 원격접속
 -- 위치 : C:\Program Files\PostgreSQL\18\data\pg_hba.conf
 
 -- 파일 하단에 작성(-- 주석은 제거하세요)
 --# 외부 접근 모두 허용
 --host    all     all             0.0.0.0/0            scram-sha-256
+
+
+-- 설정 다시로드
+select pg_reload_conf();
+
+
+
+
+
+-- 방화벽 5432 포트 허용
 
---
+++ db_sql/02_ddl.sql
@@ -0,0 +1,53 @@
+/*
+테이블 생성
+create table 테이블명(
+
+	컬럼1 자료형,
+	컬럼2 자료형,
+	컬럼3 자료형,
+	...
+);
+
+*/
+
+create table member(
+
+	pid varchar(100),
+	pname text,
+	age int8,
+	height numeric(10,2) ,
+	birth date,
+	reg_date timestamp
+);
+
+-- 테이블 이름 변경
+alter table member rename to mem;
+
+-- 테이블 컬럼명 변경
+alter table mem rename column pname to name;
+
+-- 테이블 컬렴 타입 변경
+alter table mem alter column name type varchar(50);
+
+
+-- 모든 테이블 목록보기
+select table_name from information_schema.tables;
+
+
+-- 현재 테이블 목록보기
+select table_name from information_schema.tables
+where table_schema = 'public';
+
+-- 테이블 삭제
+drop table mem;
+
+
+/*공대남방 테이블을 정의하세요 shirt  =====================
+숫자         문자     실수       날짜,      날짜시간,       정수
+
+옷번호,     제품명,   가격,       출시일,      판매일,      크기(천단위)
+*/
+
+
+
+
