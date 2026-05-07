60d8c489f07b435a3625b9d80a0c14d1ba2875e1
nemo <jx007s@gmail.com>
2026-05-07 10:58:54 +0900

    5_7_1

--- db_sql/user_db.sql
+++ db_sql/user_db.sql
@@ -11,4 +11,11 @@ create user busan_user with PASSWORD '123456' login;
 create DATABASE busan_db;
 
 -- 권한부여
+alter database busan_db owner to busan_user;
+
+-- 위치 : C:\Program Files\PostgreSQL\18\data\pg_hba.conf
+
+-- 파일 하단에 작성(-- 주석은 제거하세요)
+--# 외부 접근 모두 허용
+--host    all     all             0.0.0.0/0            scram-sha-256
 
