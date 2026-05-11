e850fa6ab5e09e05fb64d1cd7780f1df8bf6a078
nemo <jx007s@gmail.com>
2026-05-11 18:15:58 +0900

    5_11_8

---
+++ db_sql/person.sql
@@ -0,0 +1,9 @@
+CREATE TABLE person
+(
+    pid character varying ,
+    pame character varying ,
+    age integer,
+    height numeric,
+    birth date,
+    reg_date timestamp without time zone
+);
