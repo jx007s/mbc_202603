2e9997e2a875aa535b7927bc0fb9e5c3ef867068
nemo <jx007s@gmail.com>
2026-05-21 12:44:14 +0900

    5_21_4

--- java_work/springPrj/src/main/java/anno_p/ReHandPhone.java
+++ java_work/springPrj/src/main/java/anno_p/ReHandPhone.java
@@ -23,76 +23,22 @@ public class ReHandPhone {
 
     @Resource
     ReBattery bat;
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    @Override
-    public String toString() {
-        return "ReHandPhone{" +
-                "name='" + name + '\'' +
-                ", fc=" + fc +
-                ", rc=" + rc +
-                ", mm=" + mm +
-                ", dd=" + dd +
-                ", bat=" + bat +
-                '}';
-    }
 }
 @Data
 class ReCamera {    String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    @Override
-    public String toString() {
-        return "ReCamera{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
 @Data
 class ReMic{    String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    @Override
-    public String toString() {
-        return "ReMic{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
 @Data
 class ReDisplay{    String name;
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    @Override
-    public String toString() {
-        return "ReDisplay{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
 @Data
 class ReBattery{    String name;
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    @Override
-    public String toString() {
-        return "ReBattery{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
